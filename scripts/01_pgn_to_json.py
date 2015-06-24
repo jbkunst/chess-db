0f67768ce43b7009fd1cb5085b1e5e31815fe59a
Joshua <jbkunst@gmail.com>
2015-06-24 01:34:35 -0300

    adding pgn moves

--- scripts/01_pgn_to_json.py
+++ scripts/01_pgn_to_json.py
@@ -1,29 +1,38 @@
-# Converts the millionbase chess PGN database (http://www.top-5000.nl/pgn.htm) to json
-# with one json dictionary per row. (That is, the resulting file is contain multiple json objects, 
-# not just one large).
-
 import json
-import chess.pgn # From python-chess https://github.com/niklasf/python-chess
+import time
+import chess.pgn
 
-pgn = open("../data/KingBaseLite2015-05-A80-A99.pgn") # Or where you have put it
-fout = open('../data/KingBaseLite2015-05-A80-A99.json', 'w') # Or where you want it
+pgn = open("../data/KingBaseLite2015-05-A80-A99.pgn")
+fout = open('../data/KingBaseLite2015-05-A80-A99.json', 'w')
+
+t0 = time.clock()
 
 count = 0
 node = chess.pgn.read_game(pgn)
+
 while node != None:
+
   info =  node.headers
   info["fen"] = []
+  info["pgn"] = []
+
   while node.variations:
     next_node = node.variation(0)
     info["fen"].append(node.board().fen())
+    info["pgn"].append(node.board().san(next_node.move))
     node = next_node
+
   info["fen"].append(node.board().fen())
   node = chess.pgn.read_game(pgn)
+
   json.dump(info, fout, encoding='latin1')
   fout.write('\n')
   count += 1
-  if(count % 100 == 0):
+  
+  if(count % 10 == 0):
     print(count)
 
 fout.close()
-print "ready"
+
+print (time.clock() - t0)/count, "seconds per game"
+print "ready " + str(count) + " games"
