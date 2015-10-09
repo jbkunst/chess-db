88a1b21b59ea748c1c1f049ca3546777905fa795
Joshua <jbkunst@gmail.com>
2015-10-09 19:32:30 -0300

    WHAT a change made rchess :D

--- scripts/idk.R
+++ scripts/idk.R
@@ -1,45 +1,57 @@
+#### Packages ####
 rm(list = ls())
 library("magrittr")
 library("plyr")
 library("dplyr")
 library("rchess")
 library("stringr")
-
-fls <- dir("data/", pattern = ".*pgn$", full.names = TRUE)
-fls
+library("RSQLite")
 
 
-dir.create("data_pgn")
+#### Parameters ####
+PATH_PGN <- "data_pgn"
+PATH_SQL <- "data_sqlite"
+DB_NAME <- "db.sqlite"
+DB_PATH <- file.path(PATH_SQL, DB_NAME) 
+VERBOSE <- TRUE
 
-games <- ldply(fls, function(f){ # f <- "data/KingBaseLite2015-05-B20-B49.pgn"
-  print(f)
-  flines <- readLines(f)
-  if (length(flines) < 2 ) {
-    return(0)
-  }
+dir.create(PATH_PGN)
+dir.create(PATH_SQL)
+
+# remove if exist!
+if (file.exists(DB_PATH)) file.remove(DB_PATH)
+
+db <- dbConnect(SQLite(), dbname = DB_PATH)
+
+
+#### Process ####
+files_pgn <- dir("data/", pattern = ".*pgn$", full.names = TRUE)
+files_pgn
+
   
+games_by_file <- laply(files_pgn, function(f){ # f <- "data/KingBaseLite2015-05-B20-B49.pgn"
+  if(VERBOSE) message(f)
+  flines <- readLines(f)
   where_is_no_info <- which(str_length(flines) == 0)
   where_is_no_info <- where_is_no_info[seq(length(where_is_no_info)) %% 2 == 0]
   where_is_no_info <- c(0, where_is_no_info)
-  
   dfcuts <- data_frame(from = head(where_is_no_info, -1) + 1,
                        to = tail(where_is_no_info, -1) - 1)
-  
   nrow(dfcuts)
-  
 })
 
-padwidth <- nchar(max(games))
-rm(games)
+data_frame(files_pgn, games_by_file)
+summary(games_by_file)
+cumsum(games_by_file)
+padwidth <- nchar(max(games_by_file))
+rm(games_by_file)
 
 
-llply(fls, function(f){ # f <- "data/KingBaseLite2015-05-B20-B49.pgn"
+l_ply(files_pgn, function(f){ # f <- "data/KingBaseLite2015-05-A80-A99.pgn" # "data/KingBaseLite2015-05-B20-B49.pgn"
   
-  print(f)
+  if(VERBOSE) print(f)
   
   flines <- readLines(f)
-  
-  if (length(flines) < 2 ) return(FALSE)
   
   where_is_no_info <- which(str_length(flines) == 0)
   where_is_no_info <- where_is_no_info[seq(length(where_is_no_info)) %% 2 == 0]
@@ -48,24 +60,58 @@ llply(fls, function(f){ # f <- "data/KingBaseLite2015-05-B20-B49.pgn"
   dfcuts <- data_frame(from = head(where_is_no_info, -1) + 1,
                        to = tail(where_is_no_info, -1) - 1)
   
-  f2 <- str_replace_all(basename(f), "KingBaseLite2015-05-|\\.pgn", "")
+  fblock <- str_extract(basename(f), "[A-Z]\\d{2}-[A-Z]\\d{2}")
   
-  for (row in seq(nrow(dfcuts))) { # row <- 50000
-    if (row %% 10000 == 0) {
-      message( row/nrow(dfcuts) * 100)
-    }
+  df_games <- ldply(seq(nrow(dfcuts)), function(row){ # row <- 123
     
-    if (row > 100) {
-      return(FALSE)
-    }
+    fid <- str_pad(row, width = padwidth, pad = "0")
+    fname <- sprintf("%s-%s", fblock, fid)
     
-    writeLines(flines[seq(dfcuts[row, ]$from, dfcuts[row, ]$to)],
-               con = sprintf("data_pgn/%s_%s.pgn",
-                             f2,
-                             str_pad(row, width = padwidth, pad = "0")))
-  }
+    pgn <- flines[seq(dfcuts[row, ]$from, dfcuts[row, ]$to)]
+    
+    # writeLines(pgn, con = sprintf("data_pgn/%s.pgn", fname))
+    
+    ## data for moves
+    moves <- pgn[seq(which(pgn == "") + 1, length(pgn))] %>% 
+      paste0(collapse = " ") %>% 
+      str_split("\\d+\\.|\\s+") %>% 
+      unlist() %>% 
+      setdiff(c("")) %>% 
+      head(-1)
+    
+    df_moves <- data_frame(move = moves, nmove = seq(length(moves)), internalid = fname)
+    
+    dbWriteTable(conn = db, name = "games_moves", as.data.frame(df_moves),
+                 row.names = FALSE, append = TRUE)
+    
+    ## data games
+    headers <- pgn[seq(which(pgn == "")) - 1]
+    
+    data_keys <- str_extract(headers, "\\w+")
+    data_vals <- str_extract(headers, "\".*\"") %>% str_replace_all("\"", "")
+    
+    df_game <- t(data_vals) %>%
+      data.frame(stringsAsFactors = FALSE) %>%
+      setNames(data_keys) %>%
+      mutate(interalid = fname)
+    
+    df_game
+    
+  }, .progress = ifelse(VERBOSE, "text", "none")) %>% tbl_df()
   
-  TRUE
+  dbWriteTable(conn = db, name = "games", as.data.frame(df_games),
+               row.names = FALSE, append = TRUE)
+  
+}, .progress = ifelse(VERBOSE, "text", "none"))
+  
+  
+dbListTables(db)
+dbListFields(db, "games_moves")      
 
-}, .progress = "text")
+dbGetQuery(db, "select * from games_moves limit 10")
+dbGetQuery(db, "select count(1) from games_moves")
+
+
+dbDisconnect(db)
+
 
