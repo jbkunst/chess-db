c28ba8a840a5eda9c93f9d5f6ddec383689ffdda
Joshua <jbkunst@gmail.com>
2015-10-13 00:23:23 -0300

    uploading example and readme

--- scripts/02_sqlite_to_fmts.R
+++ scripts/02_sqlite_to_fmts.R
@@ -7,6 +7,7 @@ library("readr")
 
 #### Parameters ####
 PATH_TSV <- "data-tsv"
+PATH_GZIP <- "data-gzip"
 PATH_RDATA <- "data-rdata"
 PATH_SQL <- "data-sqlite"
 DB_NAME <- "db.sqlite"
@@ -14,7 +15,7 @@ DB_NAME <- "db.sqlite"
 DB_PATH <- file.path(PATH_SQL, DB_NAME) 
 
 #### Folders ####
-l_ply(c(PATH_TSV, PATH_RDATA), function(x){
+l_ply(c(PATH_TSV, PATH_RDATA, PATH_GZIP), function(x){
   unlink(x, recursive = TRUE)
   file.remove(x)
   dir.create(x)
@@ -30,14 +31,22 @@ str(games)
 
 games <- games %>% select(-id)
 
-#### Export 1 ####
+#### Export ####
 save(games, file = file.path(PATH_RDATA, "games.RData"))
 write.table(games, file = file.path(PATH_TSV, "game.tsv"), quote = FALSE, sep = "\t", row.names = FALSE)
 
 
+gz <- gzfile(file.path(PATH_GZIP, "games.tsv.gz"), "w")
+write.table(games, file = gz, quote = FALSE, sep = "\t", row.names = FALSE)
+close(gz)
 
-#### Export 2 ####
-games <- games %>% select(-moves)
 
-save(games, file = file.path(PATH_RDATA, "games_wo_moves.RData"))
-write.table(games, file = file.path(PATH_TSV, "game_wo_moves.csv"), quote = FALSE, sep = "\t", row.names = FALSE)
+#### Examples ####
+int <- readLines("data-raw/KingBase2015-09-A00-A39.pgn", n = 30)
+int <- int[seq(0, max(which(int == "")))]
+
+out <- games %>% head(10)
+
+writeLines(int, con = "example_input.pgn")
+write.table(out, file = "example_output.tsv", quote = FALSE, sep = "\t", row.names = FALSE)
+write.table(out, file = "example_output.csv", quote = FALSE, sep = ";", row.names = FALSE)
